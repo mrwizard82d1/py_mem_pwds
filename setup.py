1f405808b009d4bcd2ca921c3d9b5067adfc0d0d
Larry.Jones <mrwizard82d1@earthlink.net>
2010-07-25 22:24:59 -0500

    Continued changing directories to conform to Python package structure.
    - Added README.txt (in rst format).
    - Changed setup.py to reflect new directory structure.
    - NOTE: still have not decided how to handle data files.

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ from distutils.core import setup
 
 import os
 
-setup(name='MemorablePwds',
-      version='1.0',
-      description='Utilities to generate strong but easily memorized passwords.',
-      author='Larry Jones',
-      author_email='ljones5@slb.com',
-      packages=['', 'tkui', 'pp2e_laj.gui.tools'],
-      data_files=[('MemorablePwds', ['dict.txt']),])
- 
+setup(
+    name='MemorablePwds',
+    version='1.0',
+    author='Larry Jones',
+    author_email='mrwizard82d2@earthlink.net',
+    packages=['MemorablePwds', 'MemorablePwds.tkui'],
+    data_files=[
+        ('', ['share/MemorablePwds/DICT.TXT']),
+        ('', ['share/MemorablePwds/DICTION.ZIP']),
+        ('', ['share/MemorablePwds/TESTDICT.TXT']),
+        ],
+    url='http://www.earthlink.com/mrwizard82d1',
+    description='Generate strong but easily memorized passwords.',
+    long_description=open('README.txt').read()
+    )
