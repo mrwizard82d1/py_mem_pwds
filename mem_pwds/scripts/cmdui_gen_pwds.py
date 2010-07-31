7b86b6b9d60810384719d59f4dbd0c09b7e3ba8c
Larry Jones <mrwizard82d1@earthlink.net>
2010-07-31 01:15:56 -0500

    Moved Cmd UI to scripts sub-directory.

---
+++ mem_pwds/scripts/cmdui_gen_pwds.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+
+"""An interactive, text-based application to generate strong, easily
+memorized passwords.""" 
+
+import cmd
+import os
+import string
+import sys
+
+
+try:
+    # if it is installed
+    from mem_pwds.MemorablePwds import MemorablePwds
+except ImportError:
+    # if it is in development
+    sys.path.append(os.path.join(os.path.split(os.path.abspath(__file__))[0],
+                                               '..', '..'))
+    from mem_pwds.MemorablePwds import MemorablePwds
+
+
+class MemorablePwdsApp(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = 'Memorable Passwords: '
+        self._generator = MemorablePwds()
+
+    def help_next(self):
+        print 'Generates the next password.'
+    def do_next(self, theCount):
+        print self._generator.next()
+        
+    def help_EOF(self):
+        print 'Quits the program.'
+    def do_EOF(self, line):
+        return True
+        
+    def help_exit(self):
+        print 'Quits the program.'
+    def do_exit(self, line):
+        if __name__ == '__main__':
+            sys.exit()
+        else:
+            raise "exit"
+        
+
+def doApp():
+    theApp = MemorablePwdsApp()
+    theApp.cmdloop()
+
+
+if __name__ == '__main__':
+    doApp()
+
